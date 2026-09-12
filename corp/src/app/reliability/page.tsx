b2b79675acdaf62d38365ebd536f3c9b4b8eb30f
aghaith <abbasghayth5@gmail.com>
2026-09-12 16:53:56 +0400

    Adding Some Styling

--- corp/src/app/reliability/page.tsx
+++ corp/src/app/reliability/page.tsx
@@ -1,3 +1,12 @@
-export default function Reliability() {
-  return <div>Reliability Page</div>;
+import reliabilityImg from "public/reliability.jpg";
+import Hero from "@/components/hero";
+
+export default function ReliabilityPage() {
+  return (
+    <Hero
+      imgData={reliabilityImg}
+      imgAlt="welding"
+      title="Super high reliability hosting"
+    />
+  );
 }
--- corp/src/components/hero.tsx
+++ corp/src/components/hero.tsx
@@ -17,10 +17,10 @@ export default function Hero(props: HeroProps) {
           fill
           style={{ objectFit: "cover" }}
         />
-        <div className="absolute inset-0 bg-linear-to-r from-slat-900"></div>
+        <div className="absolute inset-0 bg-gradient-to-r from-slate-900" />
       </div>
       <div className="pt-48 flex justify-center items-center">
-        <h1 className="text-white test-6xl">{props.title}</h1>
+        <h1 className="text-white text-6xl">{props.title}</h1>
       </div>
     </div>
   );
