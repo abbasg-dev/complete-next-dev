742b8d2e404b0e13ea63c8ea58cca77213d61ba0
aghaith <abbasghayth5@gmail.com>
2026-09-12 20:57:17 +0400

    Adding a Creation Form

--- snippets/src/app/layout.tsx
+++ snippets/src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning={true}
       >
-        {children}
+        <div className="container mx-auto px-12">{children}</div>
       </body>
     </html>
   );
--- snippets/src/app/snippets/new/page.tsx
+++ snippets/src/app/snippets/new/page.tsx
@@ -1,3 +1,22 @@
 export default function SnippetCreatePage() {
-  return <div>Create a Snippet!</div>;
+  return (
+    <form className="flex flex-col gap-4">
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <label className="w-12" htmlFor="title">
+          Title
+        </label>
+        <input name="title" className="border rounded p-2 w-full" id="title" />
+      </div>
+      <div className="flex flex-col gap-4">
+        <label className="w-12" htmlFor="code">
+          Code
+        </label>
+        <textarea name="code" className="border rounded p-2 w-full" id="code" />
+      </div>
+      <button type="submit" className="rounded p-2 bg-blue-200">
+        Create
+      </button>
+    </form>
+  );
 }
